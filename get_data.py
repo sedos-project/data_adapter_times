a723505db658274a083999064469764fa9db4f09
Pathak <ac145144@u.ad.uni-stuttgart.de>
2024-04-24 09:50:29 +0200

    separated data extraction function

---
+++ get_data.py
@@ -0,0 +1,19 @@
+import requests
+import pandas as pd
+
+
+def fetch_data(url):
+
+    response = requests.get(url)
+    data = response.json()
+
+    # Convert the JSON data into a DataFrame
+    df = pd.DataFrame(data)
+
+    return df
+
+
+# Fetch Data:
+# url = "https://openenergy-platform.org/api/v0/schema/model_draft/tables/ind_steel_blafu_0/rows"
+# fetched_data = fetch_data(url)
+# print(fetched_data)
